bd3c3d570d2fcb7f3e3d5575c61df051545dd942
Pashahasband <pashajurev@mail.ru>
2019-05-14 21:15:39 +0300

    Добавил консольный тестовый проект

---
+++ test/test/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey localMachineKey = Registry.LocalMachine.OpenSubKey("SOFTWARE", true).OpenSubKey("Mozila", true).OpenSubKey("Firefox", true);
+            RegistryKey helloKey = localMachineKey.CreateSubKey("HelloKey");
+            helloKey.SetValue("URL", "localhost");
+            helloKey.Close();
+
+        }
+    }
+}
